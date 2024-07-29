8957529511df55c00273b07bbda281658090c387
monika2002782 <cb19s193335monika@gmail.com>
2024-07-29 16:13:47 +0530

    Add files via upload

---
+++ visual stdio/New folder/1_operator.py
@@ -0,0 +1,36 @@
+# # arithmetic operators
+# a=60
+# b=70
+# print(a+b)
+# tamil=int(input("enter your tamil marks:"))
+# english=int(input("enter your english marks:"))
+# maths=int(input("enter your maths marks:"))
+# total=tamil+english+maths
+# average=total/3
+# print(total)
+# print(average)
+
+#  #comparison operator
+# a=3
+# b=2
+# a>>=b
+# print(a)
+
+
+#  #identity operator 
+# a=["apple","mango"]
+# b=["apple","mango"]
+# print(a is  b)
+
+# #and operator
+# x=4
+# y=7
+# print((x>2)and(y>5))
+
+
+# #not operator
+# a=90
+# b=89
+# print(a>90)and(b<90)
+# print(not(a>89 and b>78))
+
